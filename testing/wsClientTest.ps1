e433eea12fd0f20b98a17d5279eada8f53e58ba2
Joakim Olsson <joakim.olsson@cornerstone.se>
2021-04-25 13:44:52 +0200

    Generalized wsClientTest.ps1.

--- testing/wsClientTest.ps1
+++ testing/wsClientTest.ps1
@@ -1,23 +1,32 @@
+param(
+    $hostname = "http://localhost",
+    $user = "admin",
+    $pass = 'Pa55w.rd'
+)
+
 $stateDir = "$PSScriptRoot\state"
 
 if (Test-Path $stateDir -PathType Container) {
     Remove-Item $stateDir -Recurse
 }
 
-$t = Invoke-RestMethod -Uri http://localhost/auth -Body '{"name": "admin", "password": "Pa55w.rd"}' -Method Post -ContentType application/json
+$authBody = @{ name = $user; password = $pass } | ConvertTo-Json
+
+$t = Invoke-RestMethod -Uri "$hostname/auth" -Body $authBody -Method Post -ContentType application/json
 $headers = @{ authorization="bearer $($t.token)" }
 
-$u = Invoke-RestMethod -Uri http://localhost/auth/identity/me -Method Get -Headers $headers
+$u = Invoke-RestMethod -Uri "$hostname/auth/identity/me" -Method Get -Headers $headers
 
 $u.identity.functions | Write-Host
 
 if ("api" -notin $u.identity.functions) {
-    Invoke-RestMethod -Uri "http://localhost/auth/identity/$($u.identity.id)" -Method Patch -Headers $headers -Body (@{functions = ($u.identity.functions + "api")} | ConvertTo-Json) -ContentType 'application/json'
+    $functionPatchBody = @{functions = ($u.identity.functions + "api")} | ConvertTo-Json
+    Invoke-RestMethod -Uri "$hostname/auth/identity/$($u.identity.id)" -Method Patch -Headers $headers -Body $functionPatchBody -ContentType 'application/json'
 
-    $t = Invoke-RestMethod -Uri http://localhost/auth -Body '{"name": "admin", "password": "Pa55w.rd"}' -Method Post -ContentType application/json
+    $t = Invoke-RestMethod -Uri "$hostname/auth" -Body $authBody -Method Post -ContentType application/json
     $headers = @{ authorization="bearer $($t.token)" }
 }
 
-$c = Invoke-RestMethod -Uri http://localhost/api/client/provision -Body '{"id": "FLORENSTR"}' -Method Post -ContentType application/json -Headers $headers
+$c = Invoke-RestMethod -Uri "$hostname/api/client/provision" -Body '{"id": "FLORENSTR"}' -Method Post -ContentType application/json -Headers $headers
 
 $c.token
